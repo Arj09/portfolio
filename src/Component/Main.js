840634054eeef5fd8086f079e59e25c1295ef025
Arj09 <arjunkushwah100199@gmail.com>
2023-10-12 11:20:11 +0530

    add

--- src/Component/Main.js
+++ src/Component/Main.js
@@ -5,14 +5,27 @@ import {AppBar, Box, Card, CardActionArea, CardContent, CardMedia, Grid, Typogra
 
 
 const ContainerBox = styled('div')(({theme})=>({
-    display:'grid',
-    width:"60vw",
+    width:"80%",
+    display:"grid",
+    margin:"20px auto",
+   
     gridTemplateRows:"1fr",
-    gridTemplateColumns:"1fr 1fr 1fr",
-    margin:"10px auto",
-    justifyContent:"center",
+    gridTemplateColumns:"1fr 1fr 1fr 1fr",
     alignItems:"center",
-    gap:"10px",
+    textAlign:"center",
+    gap:"30px",
+    padding:"20px",
+    backgroundColor:"white",
+    justifyContent:"space-between",
+    padding:"10px",
+    [theme.breakpoints.down('md')]:{
+        gridTemplateColumns:"1fr 1fr",
+
+    },
+    [theme.breakpoints.down('sm')]:{
+        gridTemplateColumns:"1fr",
+
+    }
    
     
 
@@ -20,7 +33,7 @@ const ContainerBox = styled('div')(({theme})=>({
 
 
 const CardBox = styled('div')(({theme})=>({
-    border:"0.2px solid red"
+    border:"0.2px solid blue"
     
 
 
@@ -28,25 +41,36 @@ const CardBox = styled('div')(({theme})=>({
 
 }))
 export const Main = ()=>{
+
+
+    let data = [1,2,3,4,5,6,6,3,7]
+    
     return(
-        <Box  sx={{width:"100vw", }}>
+        <Box  >
 
             <Typography  variant='h3' sx={{textAlign:"center", paddingTop:"50px", paddingBottom:"20px"}}>Our Work</Typography>
            <ContainerBox>
-            <CardBox>
-                <img  style={{width:"100%", height:"10%"}}  src='https://images.pexels.com/photos/16170191/pexels-photo-16170191/free-photo-of-tenerife-las-carboneras.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load://images.pexels.com/photos/11042107/pexels-photo-11042107.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'/>
-                <Box sx={{padding:"0 20px"}}>
-                    <Typography>MernStack project</Typography>
-                    <Typography variant="body2" color="text.secondary">Gwalior is a city in the central Indian state Gwalior is a city in the central Indian state </Typography>
+            {
+                data.map((data)=>{
+                    return(
+                        <CardBox>
+                            <img  style={{width:"100%", height:"10%"}}  alt='jindal' src='https://images.pexels.com/photos/16170191/pexels-photo-16170191/free-photo-of-tenerife-las-carboneras.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load://images.pexels.com/photos/11042107/pexels-photo-11042107.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'/>
+                            
+                            <Box sx={{padding:" 20px"}}>
+                                <Typography>MernStack project</Typography>
+                                <Typography variant="body2" color="text.secondary"  sx={{paddingBottom:"10px"}}>Gwalior is a city in the central Indian state Gwalior is a city in the central Indian state </Typography>
+                                <Typography   sx={{textAlign:"center", color:'white', cursor:"pointer", padding:"10px 20px", backgroundColor:"blue" }}>Link</Typography>
+                            </Box>
+                </CardBox>
+                    )
+                })
+            }
+            
 
-                </Box>
-            </CardBox>
-            <CardBox>
-                <Typography>Arjun</Typography>
-            </CardBox>
-            <CardBox>
-                <Typography>Arjun</Typography>
-            </CardBox>
+            
+
+            
+           
 
 
            </ContainerBox>
--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -2,7 +2,7 @@ import react from 'react'
 import {AppBar, Box, InputBase, Stack, Typography}  from '@mui/material'
 export const Navbar = ()=>{
     return(
-        <AppBar sx={{position:"relative",width:"100vw", height:"80px", backgroundColor:"blue", display:"flex", flexDirection:"row", justifyContent:"space-between", alignItems:"center", padding:"10px 50px", color:"white"}}>
+        <AppBar sx={{position:"relative", height:"80px", backgroundColor:"blue", display:"flex", flexDirection:"row", justifyContent:"space-between", alignItems:"center", padding:"10px 50px", color:"white"}}>
             <Typography  variant='h4'>MernStack</Typography>
             <Stack   direction={'row'} sx={{display: { xs: 'none', sm: 'block' }}}>
                 <InputBase placeholder='Search' sx={{color:"white" , border:"2px solid white", padding:"5px 10px", borderRadius:"7px"}}/>
